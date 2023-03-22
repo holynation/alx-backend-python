e31fa35ed88be0ff3d55552ffb0c0c17fbb6d1c4
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-22 12:03:04 +0100

    feat: Added python backend code

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This ia a type-annotated function that returns the sum of two floats """
+
+
+def add(a: float, b: float) -> float:
+    """adds two floats and return sum as float"""
+    return a + b
